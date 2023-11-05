88e2af15cbdae2d5684ee85167264aee34c51a44
MariaMutiloa <maria.mutiloa@opendeusto.es>
2023-11-05 15:04:06 +0100

    funciona

--- AloTleDelisia/myapp/models.py
+++ AloTleDelisia/myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    dni = models.CharField(max_length=9)
+    nombre = models.CharField(max_length=15)
+    apellidos=models.TextField()
+    fecha_nac = models.DateTimeField('Fecha de nacimiento')
+    correo=models.EmailField()
+    direccion=models.TextField()
