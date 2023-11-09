39689c8da5e1b9f73739194cd58c3171becbde44
MariaMutiloa <maria.mutiloa@opendeusto.es>
2023-11-09 20:27:34 +0100

    primera pagina hecha

--- AloTleDelisia/AloTleDelisia/urls.py
+++ AloTleDelisia/AloTleDelisia/urls.py
@@ -20,6 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myapp/', include('myapp.urls'))
+    path('myapp/', include('myapp.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
--- AloTleDelisia/myapp/views.py
+++ AloTleDelisia/myapp/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-@login_required
 def index(request):
-    return render(request, 'myapp/base.html')
+    return render(request, 'myapp/primera.html')
 
