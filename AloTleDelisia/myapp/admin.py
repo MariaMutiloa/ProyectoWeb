a48efaa2df934c74b28eca19279f1a6a9fdea873
MariaMutiloa <maria.mutiloa@opendeusto.es>
2023-11-13 16:43:54 +0100

    modelos añadidos a administrador

--- AloTleDelisia/myapp/admin.py
+++ AloTleDelisia/myapp/admin.py
@@ -2,3 +2,12 @@ from django.contrib import admin
 
 
 # Register your models here.
+admin.site.register(Pedido)
+admin.site.register(TipoProducto)
+admin.site.register(Producto)
+admin.site.register(TipoRestaurante)
+admin.site.register(Restaurante)
+admin.site.register(Menu)
+admin.site.register(PedidoProducto)
+admin.site.register(ProductoMenu)
+
