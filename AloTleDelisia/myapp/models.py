a0baa125dac246cca2ca579039a67ecfb3c862f9
MariaMutiloa <maria.mutiloa@opendeusto.es>
2023-11-15 09:32:23 +0100

    mediante views la carga de categorias

--- AloTleDelisia/myapp/models.py
+++ AloTleDelisia/myapp/models.py
@@ -8,22 +8,6 @@ class Pedido(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE)
     id_restaurante = models.ForeignKey('Restaurante', on_delete=models.CASCADE)
 
-
-class TipoProducto(models.Model):
-    idTipoProducto = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=255)
-    descripcion=models.TextField()
-
-
-class Producto(models.Model):
-    idProducto = models.AutoField(primary_key=True)
-    nombre = models.CharField(max_length=255)
-    descripcionProducto = models.TextField()
-    precio = models.DecimalField(max_digits=10, decimal_places=2)
-    idTipoProducto = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
-    idRestaurante = models.ForeignKey('Restaurante', on_delete=models.CASCADE)
-
-
 class TipoRestaurante(models.Model):
     idTipoRestaurante = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=255)
@@ -36,6 +20,23 @@ class Restaurante(models.Model):
     horario = models.CharField(max_length=255)
     descripcion = models.TextField()
     idTipoRestaurante = models.ForeignKey(TipoRestaurante, on_delete=models.CASCADE)
+
+
+class TipoProducto(models.Model):
+    idTipoProducto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    descripcion=models.TextField()
+    idRestaurante = models.ForeignKey(Restaurante, on_delete=models.CASCADE)
+
+
+class Producto(models.Model):
+    idProducto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=255)
+    descripcionProducto = models.TextField()
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    idTipoProducto = models.ForeignKey(TipoProducto, on_delete=models.CASCADE)
+    idRestaurante = models.ForeignKey('Restaurante', on_delete=models.CASCADE)
+
 
 class Menu(models.Model):
     idMenu = models.AutoField(primary_key=True)
