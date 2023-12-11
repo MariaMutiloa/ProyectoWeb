7ce8946348b29da7a1bffdd3425140f78cc23616
MariaMutiloa <maria.mutiloa@opendeusto.es>
2023-12-11 21:11:13 +0100

    buscador

--- AloTleDelisia/myapp/static/js/producto.js
+++ AloTleDelisia/myapp/static/js/producto.js
@@ -1,22 +1,21 @@
 function filtrarProductos() {
-    var textoBuscado = document.querySelector('.buscar').value.toLowerCase();
-    var productos = document.querySelectorAll('.item');
-  
-    for (var i = 0; i < productos.length; i++) {
-      var nombreProducto = productos[i].querySelector('.titulo-item').textContent.toLowerCase();
-      var coincideExactamente = nombreProducto === textoBuscado;
-      var contieneTexto = nombreProducto.includes(textoBuscado);
-  
-      if (textoBuscado === '' || coincideExactamente || contieneTexto) {
-        productos[i].style.display = 'block';
-      } else {
-        productos[i].style.display = 'none';
-      }
+  var textoBuscado = document.querySelector('.buscar').value.toLowerCase();
+  var productos = document.querySelectorAll('.item');
+
+  for (var i = 0; i < productos.length; i++) {
+    var nombreProducto = productos[i].querySelector('.titulo-item').textContent.toLowerCase();
+    var coincideExactamente = nombreProducto === textoBuscado;
+    var contieneTexto = nombreProducto.includes(textoBuscado);
+
+    if (textoBuscado === '' || coincideExactamente || contieneTexto) {
+      productos[i].style.display = 'block';
+    } else {
+      productos[i].style.display = 'none';
     }
   }
-  
-  document.getElementById('btnBuscar').addEventListener('click', function() {
-    console.log('Se ha hecho clic en el botón de búsqueda');
-    filtrarProductos();
-  });
-  
+}
+
+document.getElementById('btnBuscar').addEventListener('click', function() {
+  console.log('Se ha hecho clic en el botón de búsqueda');
+  filtrarProductos();
+});
--- AloTleDelisia/myapp/static/js/restaurante.js
+++ AloTleDelisia/myapp/static/js/restaurante.js
@@ -1,42 +1,41 @@
 function ocultarTodosLosRestaurantes() {
-    var restaurantes = document.getElementsByClassName('restaurante');
-    for (var i = 0; i < restaurantes.length; i++) {
-      mostrarRestaurante(restaurantes[i]); // Mostrar todos los restaurantes
+  var restaurantes = document.getElementsByClassName('restaurante');
+  for (var i = 0; i < restaurantes.length; i++) {
+    mostrarRestaurante(restaurantes[i]); // Mostrar todos los restaurantes
+  }
+}
+
+function ocultarRestaurante(restaurante) {
+  restaurante.style.display = 'none';
+}
+
+function mostrarRestaurante(restaurante) {
+  restaurante.style.display = 'inline-block';
+}
+
+function filtrarRestaurantes() {
+  var textoBuscado = document.getElementById('inputBuscar').value.toLowerCase();
+  var restaurantes = document.getElementsByClassName('restaurante');
+
+  if (textoBuscado === '') {
+    // Si el campo de búsqueda está vacío, mostrar todos los restaurantes
+    ocultarTodosLosRestaurantes();
+    return;
+  }
+
+  for (var i = 0; i < restaurantes.length; i++) {
+    var restauranteNombre = restaurantes[i].textContent.toLowerCase();
+    var coincideExactamente = restauranteNombre === textoBuscado;
+    var contieneTexto = restauranteNombre.includes(textoBuscado);
+
+    if (coincideExactamente || contieneTexto) {
+      mostrarRestaurante(restaurantes[i]);
+      console.log('Restaurante encontrado:', restaurantes[i].textContent);
+    } else {
+      ocultarRestaurante(restaurantes[i]);
     }
   }
-  
-  function ocultarRestaurante(restaurante) {
-    restaurante.style.display = 'none';
-  }
-  
-  function mostrarRestaurante(restaurante) {
-    restaurante.style.display = 'inline-block';
-  }
-  
-  function filtrarRestaurantes() {
-    var textoBuscado = document.getElementById('inputBuscar').value.toLowerCase();
-    var restaurantes = document.getElementsByClassName('restaurante');
-  
-    if (textoBuscado === '') {
-      // Si el campo de búsqueda está vacío, mostrar todos los restaurantes
-      ocultarTodosLosRestaurantes();
-      return;
-    }
-  
-    for (var i = 0; i < restaurantes.length; i++) {
-      var restauranteNombre = restaurantes[i].textContent.toLowerCase();
-      var coincideExactamente = restauranteNombre === textoBuscado;
-      var contieneTexto = restauranteNombre.includes(textoBuscado);
-  
-      if (coincideExactamente || contieneTexto) {
-        mostrarRestaurante(restaurantes[i]);
-        console.log('Restaurante encontrado:', restaurantes[i].textContent);
-      } else {
-        ocultarRestaurante(restaurantes[i]);
-      }
-    }
-    console.log('El botón se ha hecho clic');
-  }
-  
-  document.getElementById('btnBuscar').addEventListener('click', filtrarRestaurantes);
-  
+  console.log('El botón se ha hecho clic');
+}
+
+document.getElementById('btnBuscar').addEventListener('click', filtrarRestaurantes);
