d53fd50fb7896ac7009ebbe5c700258975df8acf
Adrian Martinez Navarro <adrianmartinez@opendeusto.es>
2023-12-12 20:19:07 +0100

    URLS translate

--- AloTleDelisia/myapp/urls.py
+++ AloTleDelisia/myapp/urls.py
@@ -2,19 +2,20 @@ from django.urls import path
 from .views import index
 from .views import categoria, listaRestaurantes,cerrar,restaurante,guardar_pedido,login,register_view,pedidoUsuario
 from django.views.i18n import set_language
+from django.utils.translation import gettext_lazy as _
 
 urlpatterns = [
     path('', index, name='index'),
-    path('principal', categoria, name='principal'),
+    path(_('principal'), categoria, name='principal'),
     path('login/', login, name='login'),
-    path('cerrar-sesion/', cerrar, name='cerrar'),
-    path('restaurantes/', listaRestaurantes, name='restaurantes'),
+    path(_('cerrar-sesion/'), cerrar, name='cerrar'),
+    path(_('restaurantes/'), listaRestaurantes, name='restaurantes'),
     path('restaurantes/<str:idTipoRestaurante>', listaRestaurantes, name='listaRestaurantes'),
-    path('restaurante/', restaurante, name='unRestaurante'),
+    path(_('restaurante/'), restaurante, name='unRestaurante'),
     path('restaurante/<str:nombreRestaurante>', restaurante, name='restaurante'),
-    path('pedidos/guardar', guardar_pedido, name='guardar_pedido'),
-    path('registro/',register_view , name='registro'),
+    path(_('pedidos/guardar'), guardar_pedido, name='guardar_pedido'),
+    path(_('registro/'),register_view , name='registro'),
     path('set_language/', set_language, name='set_language'),
-    path('miPerfil/', pedidoUsuario, name='usuario')
+    path(_('miPerfil/'), pedidoUsuario, name='usuario')
 ]
 
